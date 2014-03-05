daa44586fcad4bfd8665c7a2212f2b687a858e4c
Miguel Angel Martinez <miguel@beetailer.com>
2014-03-05 10:21:19 -0800

    Adding medium size image

--- app/assets/javascripts/models/product.js
+++ app/assets/javascripts/models/product.js
@@ -3,5 +3,6 @@ App.Product = DS.Model.extend({
   created_at: attr(),
   title: attr('string'),
   picture: attr('string'),
-  picture_thumb: attr('string')
+  picture_thumb: attr('string'),
+  picture_medium: attr('string')
 });
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,11 @@
 class ProductSerializer < ActiveModel::Serializer
- attributes :id, :created_at, :title, :picture_thumb
+ attributes :id, :created_at, :title, :picture_thumb, :picture_medium
 
  def picture_thumb
-   object.picture.url(:thumb)
+   object.picture.url(:thumb, false)
+ end
+
+ def picture_medium
+   object.picture.url(:medium, false)
  end
 end
