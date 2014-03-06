f23e7be00c5392797eec65ad55238053fcd3b2da
Miguel Angel Martinez <miguel@beetailer.com>
2014-03-05 19:19:38 -0800

    Adding line items

--- app/assets/javascripts/controllers/index_controller.js
+++ app/assets/javascripts/controllers/index_controller.js
@@ -7,6 +7,16 @@ App.IndexController = Ember.ObjectController.extend({
         console.info('Order created');
         //controller.transitionToRoute('order', order);
       });
+    },
+    addLineItem: function(product) {
+      order = this.get('model');
+      console.warn(order)
+      console.warn(product)
+      /* Search for a line item, if it does not exist create it 
+      * TODO*/
+      this.store.createRecord("lineItem", {product: product, order: order}).save().then(function(li){
+        console.warn("Created");
+      })
     }
   }
 });
---
+++ app/assets/javascripts/views/order_product_item.js
@@ -0,0 +1,4 @@
+App.OrderProductItemView = Ember.View.extend({
+  templateName: 'order_product_item',
+  classNames: ['col-sm-4', 'product_item'],
+});
