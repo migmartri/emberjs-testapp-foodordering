643cd52ec9924f83a8f0a8faca9a270934b9ce23
Miguel Angel Martinez <miguel@beetailer.com>
2014-03-06 19:13:39 -0800

    Adding hover

--- app/assets/javascripts/views/order_product_item.js
+++ app/assets/javascripts/views/order_product_item.js
@@ -1,4 +1,10 @@
 App.OrderProductItemView = Ember.View.extend({
   templateName: 'order_product_item',
   classNames: ['col-sm-4', 'product_item'],
+  mouseEnter: function(){
+    this.set('hovered', true);
+  },
+  mouseLeave: function(){
+    this.set('hovered', false);
+  }
 });
