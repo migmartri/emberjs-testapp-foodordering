b46cf392af437f18c0536b797521d4fc9d623a1d
Miguel Angel Martinez <miguel@beetailer.com>
2014-03-21 20:14:39 -0700

    Trying new placeholder method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :default_url => ActionController::Base.helpers.asset_path("missing/product/:style/picture.png")
+    :default_url => ':placeholder'
 
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   #validates_attachment_file_name :picture, :matches => [/png\Z/, /jpe?g\Z/]
@@ -9,4 +9,8 @@ class Product < ActiveRecord::Base
   validates :title, :company_id, presence: true
 
   has_many :line_items
+
+  Paperclip.interpolates(:placeholder) do |attachment, style|
+    ActionController::Base.helpers.asset_path("missing/product/#{style}/picture.png")
+  end
 end
