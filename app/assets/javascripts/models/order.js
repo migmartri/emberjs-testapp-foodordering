c2b6ede30fe3f9310124734cfa8d2a2c68690a5b
Miguel Angel Martinez <miguel@beetailer.com>
2014-03-25 11:40:10 -0700

    Fixes #20

--- app/assets/javascripts/models/order.js
+++ app/assets/javascripts/models/order.js
@@ -6,11 +6,5 @@ App.Order = DS.Model.extend({
   closed_at: attr(),
   order_number: attr(),
   aasm_state: attr(),
-  lineItemsCount: (function(){
-    var sum = 0;
-    this.get('line_items').forEach(function(li){
-      sum = sum + li.get('qty'); 
-    });
-    return sum;
-  }).property('line_items.@each.qty')
+  lineItemsCount: attr()
 });
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
- attributes :id, :created_at, :closed_at, :aasm_state, :order_number
+ attributes :id, :created_at, :closed_at, :aasm_state, :order_number, :line_items_count
  has_many :line_items, embed: :id, key: :line_item_ids, include: true
  has_many :suggestions, embed: :id, key: :suggestion_ids, include: true
  #has_many :products, embed: :id, key: :product_ids, include: true
@@ -10,5 +10,9 @@ class OrderSerializer < ActiveModel::Serializer
 
  def include_suggestions?
    scope && scope[:include_suggestions]
+ end
+
+ def line_items_count
+   self.line_items.to_a.sum(&:qty)
  end
 end
