dd6b68d02a2a47cce1dc9d7d135a61d80f1c3616
Miguel Angel Martinez <miguel@beetailer.com>
2014-04-15 18:07:19 -0700

    Prev and Next buttons

--- app/assets/javascripts/views/pagination_view.js
+++ app/assets/javascripts/views/pagination_view.js
@@ -5,19 +5,44 @@ App.PaginationView = Ember.View.extend({
     this.rerender();
   }.observes('page'),
   render: function(buffer){
-    for (var i=1; i<=this.get('totalPages'); i++) {
+    var totalPages = this.get('totalPages');
+    var currentPage = this.get('page');
+
+    if (totalPages == 1) return;
+
+    /* Prev page */
+    buffer.push("<li");
+    if(currentPage == 1) buffer.push(" class='disabled'");
+    buffer.push("><a href='#' data-page='prev'>&laquo;</a></li>");
+
+    for (var i=1; i<=totalPages; i++) {
       buffer.push("<li ");
-      if(this.get('page') == i){
+      if(currentPage == i){
         buffer.push(" class='active'");
       }
-      buffer.push("><a href='#'>"+ i +"</a></li>");
+      buffer.push("><a href='#' data-page='" + i + "'>"+ i +"</a></li>");
     }
+    /* Next page */
+    buffer.push("<li");
+    if(currentPage == totalPages) buffer.push(" class='disabled'");
+    buffer.push("><a href='#' data-page='next'>&raquo;</a></li>");
   },
   click: function(e){
     var $target = $(e.target);
     // Return if something in the View was clicked on that wasn't a button
-    if (!$target.is('a')) return;
-    this.loadPage($target.text());
+    if (!$target.is('a') || $target.parent().hasClass('disabled')) return;
+    var linkContent = $target.data('page');
+    switch(linkContent){
+      case 'prev':
+        this.prevPage();
+        break;
+      case 'next':
+        this.nextPage();
+        break;
+      default:
+        this.loadPage(linkContent);
+        break;
+    }
     return false; /* Stop propagation */
   },
   loadPage: function(pageNumber){
